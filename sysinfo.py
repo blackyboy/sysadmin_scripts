f39e0a394ba16f9f2434052d7cebf7ab29f49192
blackyboy <babinlonston@live.com>
2017-08-13 18:06:33 +0530

    System information using Python Script

---
+++ sysinfo.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+#A System Information Gathering Script
+import subprocess
+#Today's date and time
+subprocess.call("date", shell=True)
+print "\n"
+#Check Kernel version
+un = "uname"
+un_arg = "-a"
+print "Gathering system information with %s command:\n" % un
+subprocess.call([un, un_arg])
+print "\n"
+
+#check for current disk space
+dsk = "df"
+dsk_arg = "-h"
+print "Gathering diskspace information %s command:\n" % dsk
+subprocess.call([dsk, dsk_arg])
+print "\n"
+
