d5700d74a38327e259535a0e9e15e05515def9c7
blackyboy <babinlonston@live.com>
2017-08-13 13:07:07 +0530

    example of Python

---
+++ examples_python.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+#for Loop
+for a in [1,2]:
+	for b in ['a', 'b']:
+		print a, b
+
+#if/else condition
+
+#!/usr/bin/env python
+import os
+if os.path.isdir("tmp"):
+	print "tmp is directory"
+else:
+	print "/tmp is not a directory"
