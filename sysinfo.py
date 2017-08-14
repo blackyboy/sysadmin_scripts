db6bf649251b4b5d7811dd079b730af49d573037
blackyboy <babinlonston@live.com>
2017-08-14 23:45:34 +0530

    System information using Python Script

--- sysinfo.py
+++ sysinfo.py
@@ -10,11 +10,10 @@ un_arg = "-a"
 print "Gathering system information with %s command:\n" % un
 subprocess.call([un, un_arg])
 print "\n"
-
 #check for current disk space
 dsk = "df"
 dsk_arg = "-h"
 print "Gathering diskspace information %s command:\n" % dsk
 subprocess.call([dsk, dsk_arg])
 print "\n"
-
+subprocess.call("hostname", shell=True)
