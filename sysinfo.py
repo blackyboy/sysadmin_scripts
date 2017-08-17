ebf3724694e6c146661bfb75a4f818c8ecd62669
blackyboy <babinlonston@live.com>
2017-08-17 09:22:47 +0530

    System Information Python Script

--- sysinfo.py
+++ sysinfo.py
@@ -3,19 +3,30 @@
 import subprocess
 #Today date
 subprocess.call("date", shell=True)
+#List of functions
 def uname_func():
 	uname = "uname"
 	u_arg = "-a"
 	print "Gathering system information with %s command:\n" % uname
 	subprocess.call([uname,u_arg])
+	print "----------------------------------------------"
 
 def disk_func():
 	diskspace = "df"
 	diskspace_arg = "-h"
 	print "Gathering system information with %s command:\n" % diskspace
 	subprocess.call([diskspace, diskspace_arg])
+	print "----------------------------------------------"
+
+def host_ip():
+	host = "hostname"
+	ip   = "-l"
+	print "Gathering system information with %s command:\n" % host
+	subprocess.call([host, ip])
+	print "----------------------------------------------"
 
 def main():
 	uname_func()
 	disk_func()
+	host_ip()
 main()
